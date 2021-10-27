c554a0c220d4a148a0c703f7b3b8673d5e332f2f
anshulbadhani <84536974+anshulbadhani@users.noreply.github.com>
2021-10-27 19:27:50 +0530

    Update myImportFile.py

--- myImportFile.py
+++ myImportFile.py
@@ -1,10 +1,10 @@
 # we can also impoert iur own header file to import useful functions
 
 def helloUser(name):
-    print("Hello", name)
-def add(a, b):
+    print("Hello", name) #This function returns Hello and value of variable name
+def add(a, b): # adds a & b
     sum = a + b
     return sum
 
-if __name__ == "__main__":
+if __name__ == "__main__": #to check
     print(add(2,3))
